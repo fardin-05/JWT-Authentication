96791dd621efcfb8f50e61f0e1b0327e335d27af
Fardin Khan <azimahmed9722@gmail.com>
2026-04-28 22:37:59 +0600

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 import random
-#=============Custom User Manager=================
+
 class UserManager (BaseUserManager):
     def create_user(self, email, full_name, password = None, **extra_fields):
         if not email:
